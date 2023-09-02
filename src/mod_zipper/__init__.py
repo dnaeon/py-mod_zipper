827ba84f046fbb13bf231d3cc5a8cad943c93117
Marin Atanasov Nikolov <dnaeon@gmail.com>
2023-09-02 18:28:01 +0300

    Refactor the mod_zipper module a bit

--- src/mod_zipper/__init__.py
+++ src/mod_zipper/__init__.py
@@ -1,5 +1,4 @@
-#
-# Copyright (c) 2013-2014 Marin Atanasov Nikolov <dnaeon@gmail.com>
+# Copyright (c) 2013-2023 Marin Atanasov Nikolov <dnaeon@gmail.com>
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -22,15 +21,17 @@
 # THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 # THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
 
 """
-mod_zipper is an Apache module which is being used for browsing Zip files from your browser.
+mod_zipper is an Apache module which is being used for browsing of
+Zip archives.
 
-It allows you to download individual files from a Zip archive located on a remote system without the
-need to download first the archive locally and extract it. 
+It allows you to download individual files from a Zip archive located
+on a remote system without the need to download first the archive
+locally and extract it.
 
-The mod_zipper module requires your Apache server to have mod_python loaded already.
+The mod_zipper module requires your Apache server to have mod_python
+loaded already.
 
 """
 
@@ -41,6 +42,7 @@ import time
 import zipfile
 import tempfile
 import shutil
+
 from mod_python import util, apache
 
 _layout = """<!DOCTYPE html>
@@ -58,155 +60,132 @@ _layout = """<!DOCTYPE html>
 
     <div class="container">
     <!-- Content goes here --!>
-    %s
+    {content}
     <!-- Content ends here --!>
     </div>
   </body>
 </html>
 """
 
-def get_zip_contents(req):
+def display_zip_contents(req):
     """
     Opens a Zip archive file and displays it's contents.
-    
     """
+    zip_file = None
     try:
-        myFile = zipfile.ZipFile(req.filename, 'r', allowZip64=True)
+        zip_file = zipfile.ZipFile(req.filename, 'r', allowZip64=True)
     except IOError as e:
-        return 'Failed to open file %s: %s' % (req.filename, e)
-    
+        raise apache.SERVER_RETURN(apache.HTTP_INTERNAL_SERVER_ERROR)
+
     req.content_type = 'text/html'
-    req.send_http_header()
+    file_name = os.path.basename(req.filename)
 
-    archive_file = os.path.basename(req.filename)
-    
     data = """
-	<a href="?fetch=1">Download archive %s</a>
-	<h3>Contents of %s</h3>
-	<table>
-	  <tr>
+        <a href="?fetch=1">Download archive %s</a>
+        <h3>Contents of %s</h3>
+        <table>
+          <tr>
             <th>Filename</th>
-	    <th>Length</th>
-	    <th>Size</th>
-	    <th>Ratio</th>
-	    <th>Date</th>
-	    <th>CRC</th>
-	    <th>Comment<th>
-	  </tr>""" % (archive_file, archive_file)
+            <th>Length</th>
+            <th>Size</th>
+            <th>Ratio</th>
+            <th>Date</th>
+            <th>CRC</th>
+            <th>Comment<th>
+          </tr>""" % (file_name, file_name)
 
-    for eachMember in myFile.namelist():
-        info = myFile.getinfo(eachMember)
-
+    for item in zip_file.namelist():
+        info = zip_file.getinfo(item)
         if info.file_size and info.compress_size:
             ratio = str(round(float(info.compress_size) / float(info.file_size) * 100, 2)) + '%'
         else:
             ratio = '0%'
 
-        data += """
-	    <tr>
-		<td><a href="%s?file=%s">%s</a></td>
-        	<td>%s</td>
-        	<td>%s</td>
-            	<td>%s</td>
- 		<td>%s</td>
-		<td>%s</td>
-		<td>%s</td>
-            </tr>""" % (req.uri, info.filename, info.filename,
-                        info.file_size,
-                        info.compress_size,
-                        ratio,
-                        time.strftime('%Y-%m-%d %H:%M:%S', (info.date_time + (0, 0, 0))),
-                        hex(info.CRC),
-                        info.comment.decode('utf-8'))
-            
+        data += '''
+            <tr>
+                <td><a href="{uri}?file={file_name}">{file_name}</a></td>
+                <td>{file_size}</td>
+                <td>{compress_size}</td>
+                <td>{ratio}</td>
+                <td>{timestamp}</td>
+                <td>{crc}</td>
+                <td>{comment}</td>
+            </tr>'''.format(
+                uri=req.uri,
+                file_name=info.filename,
+                file_size=info.file_size,
+                compress_size=info.compress_size,
+                ratio=ratio,
+                timestamp=time.strftime('%Y-%m-%d %H:%M:%S', (info.date_time + (0, 0, 0))),
+                crc=hex(info.CRC),
+                comment=info.comment.decode('utf-8'),
+            )
+
     data += '</table>'
-    myFile.close()
-    req.write(_layout % data)
-    
+    zip_file.close()
+    req.write(_layout.format(content=data))
+
     return apache.OK
 
-def download_file(req, name):
-    """
-    Extracts a file from a Zip archive and transmits it to the user.
 
+def download_file_from_zip(req, name):
     """
+    Extracts a file from a Zip archive and sends it to the client
+    """
+    zip_file = None
     try:
-        myFile = zipfile.ZipFile(req.filename, 'r', allowZip64=True)
+        zip_file = zipfile.ZipFile(req.filename, 'r', allowZip64=True)
     except IOError as e:
-        return 'Failed to open file %s: %s' % (req.filename, e)
+        raise apache.SERVER_RETURN(apache.HTTP_INTERNAL_SERVER_ERROR)
 
-    # Send headers
+    # Set headers
     req.content_type = 'text/file'
-    req.headers_out['Content-Disposition'] = 'attachment; filename=%s' % os.path.basename(name.value)
+    req.headers_out['Content-Disposition'] = 'attachment; filename={}'.format(os.path.basename(name.value))
 
     # Temp directory to extract the file
-    tmpdir = tempfile.mkdtemp(dir='/tmp')
-    
-    # Extract file from the archive
-    path = myFile.extract(name.value, path=tmpdir)
-    myFile.close()
+    tmpdir = tempfile.mkdtemp(prefix='mod_zipper-')
+    path = zip_file.extract(name.value, path=tmpdir)
+    zip_file.close()
 
-    # Send the file
+    # Send the file and clean up
     send_file(req, path)
-    
-    # Clean up a bit here
-    shutil.rmtree(tmpdir)
+    shutil.rmtree(tmpdir, ignore_errors=True)
 
     return apache.OK
 
 
-def download_archive(req):
+def download_full_archive(req):
     """
-    Sends the whole Zip archive to the client
-
+    Sends the full Zip archive to the client
     """
     if not os.path.exists(req.filename):
-        return 'File %s does not exists' % req.filename
-    
-    # Send headers
-    req.content_type = 'text/file'
-    req.headers_out['Content-Disposition'] = 'attachment; filename=%s' % os.path.basename(req.filename)
+        return apache.DECLINED
 
+    req.content_type = 'text/file'
+    req.headers_out['Content-Disposition'] = 'attachment; filename={}'.format(os.path.basename(req.filename))
     send_file(req, req.filename)
     
-def send_file(req, path, chunksize=1024):
+
+def send_file(req, path):
     """
     Sends a file to the client
-
     """
-    offset   = 0
-    length   = 0
-    filesize = os.stat(path)[6]
+    return req.sendfile(path)
 
-    while filesize > 0:
-        if chunksize > filesize:
-            length += filesize
-        else:
-            length += chunksize
-
-        sent = req.sendfile(path)
-
-        offset += sent
-        filesize -= sent
 
 def handler(req):
     """
-    Apache handler for handling user requests.
-
+    The Apache handler for mod_zipper
     """
     form = util.FieldStorage(req, keep_blank_values=1)
-
-
-
-    fetch_file    = form.getfirst('file')
+    fetch_file = form.getfirst('file')
     fetch_archive = form.getfirst('fetch')
 
     if fetch_file:
-        download_file(req, fetch_file)
+        download_file_from_zip(req, fetch_file)
     elif fetch_archive:
-        download_archive(req)
+        download_full_archive(req)
     else:
-        get_zip_contents(req)
+        display_zip_contents(req)
 
     return apache.OK
-
